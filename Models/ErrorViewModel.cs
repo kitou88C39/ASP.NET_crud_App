e3cc9d2a21ef5ddd306ae3b4386fc5321c4d7f8a
kitou88C39 <kanfutrooper@icloud.com>
2023-10-19 16:16:27 +0900

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace asp.net_traning.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
